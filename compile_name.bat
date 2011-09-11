1fb6b38a662c123e7a5b86a18d48cbfa1a61eba6
Unixander <unixander@gmail.com>
2011-09-11 16:20:47 +0300

    Upgrade Compile Name

--- compile_name.bat
+++ compile_name.bat
@@ -1,14 +1,26 @@
 @echo off
+echo /////////////////////////////////////////////
+echo //        Compile Helper v 1.0             //
+echo //author: unixander                        //
+echo //site: unixander.org.ua                   //
+echo //Description:                             //
+echo //Compile .asm files with tasm or masm     //
+echo /////////////////////////////////////////////
 if exist *.obj del *.obj
 if exist *.com del *.com
 if exist *.map del *.map
-
 Set /p filename="Enter filename:"
+echo Choose compiler:
+echo 1-tasm
+echo 2-masm
+Set /p comp=">"
+if %comp%==2 goto masm
 :recompile
  tasm %filename%.asm
 tlink %filename%.obj -t
-:: Меню
+:: Menu
 :repeat
+echo ===================================
 echo Please pick a number and hit Enter:
 echo 1 - Run
 echo 2 - Delete
@@ -17,25 +29,65 @@ echo 4 - Recompile
 echo 5 - Exit to console
 echo 6 - Exit to Windows
 
-:: Выбор
-Set /p choic=""
-:: Выполнение
-IF %choic%==0 (tasm %filename%.asm
-tlink %filename%.obj -t
+:: Make choice
+Set /p choice=">"
+cls
+:: Executing
+IF %choice%==0 (tasm %filename%.asm
+tlink -t %filename%.obj 
 )
-IF %choic%==1 %filename%.com
-::удаление мусора
-IF %choic%==2 (del %filename%.obj 
+IF %choice%==1 %filename%.com
+::Delete garbage
+IF %choice%==2 (del %filename%.obj 
 del %filename%.com)
-::Перемещение в архив
-IF %choic%==3 ( del %filename%.obj
+::Move to archive
+IF %choice%==3 ( del %filename%.obj
 del %filename%.com 
 move %filename%.asm \files 
 )
-::Перекомпиляция
-IF %choic%==4 GOTO :recompile
-IF %choic%==5 cmd
-IF %choic%==6 exit
+::Recompile
+IF %choice%==4 GOTO :recompile
+IF %choice%==5 cmd
+IF %choice%==6 exit
 pause
 GOTO :repeat 
 
+:: MASM
+:masm
+Echo Enter full path
+Set /p path=">"
+:: Menu
+:repeat_masm
+echo ===================================
+echo Please pick a number and hit Enter:
+echo 1 - Run
+echo 2 - Delete
+echo 3 - Move to archive
+echo 4 - Recompile
+echo 5 - Exit to console
+echo 6 - Exit to Windows
+
+:: Make choice
+Set /p choice=">"
+cls
+:: Executing
+IF %choice%==0 (%path%\bin\ml /c /coff %filename%.asm
+%path%\bin\PoLink /SUBSYSTEM:WINDOWS %filename%.obj 
+)
+IF %choice%==1 %filename%.exe
+::Delete garbage
+IF %choice%==2 (del %filename%.obj 
+del %filename%.exe)
+::Move to archive
+IF %choice%==3 ( del %filename%.obj
+del %filename%.exe 
+move %filename%.asm \files 
+)
+::Recompile
+IF %choice%==4 (%path%\bin\ml /c /coff %filename%.asm
+%path%\bin\PoLink /SUBSYSTEM:WINDOWS %filename%.obj 
+)
+IF %choice%==5 cmd
+IF %choice%==6 exit
+pause
+GOTO :repeat_masm
